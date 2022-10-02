3071cc0e4ef798b5931f0deea09edade7b37d5b9
AytacSafarova <100721454+AytacSafarova@users.noreply.github.com>
2022-10-02 10:28:17 +0400

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,22 +1,4 @@
-// export const getDataReducer = (
-//   listOfUsers = { data: null, status: "idle", error: null },
-//   action
-// ) => {
-//   return action.type == "GET_DATA" ? action.payload : listOfUsers;
-// };
 
-// export const getDataReducer = ( action,
-// ) => {
-// console.log(action);
-//     switch (action.type) {
-//       case "GET_DATA":
-//         return action.payload
-  
-  
-//       default:
-//         return ;
-//     }
-//   };
 export const getDataReducer = (state = [], action) => {
   console.log("action", action);
   switch (action.type) {
